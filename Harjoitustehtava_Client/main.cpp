639896a374d46ebbab3b9124d5b6e0258f1087ad
Esko Haila <eracage@gmail.com>
2015-11-27 14:47:43 +0200

    removed stuff

--- Harjoitustehtava_Client/main.cpp
+++ Harjoitustehtava_Client/main.cpp
@@ -1,10 +1,11 @@
 #define WIN32_LEAN_AND_MEAN
 
-#include <windows.h>
 #include <winsock2.h>
 #include <ws2tcpip.h>
-#include <stdlib.h>
-#include <stdio.h>
+//#include <stdlib.h>
+//#include <stdio.h>
+
+#include <iostream>
 
 
 // Need to link with Ws2_32.lib, Mswsock.lib, and Advapi32.lib
@@ -16,7 +17,7 @@
 #define DEFAULT_BUFLEN 512
 #define DEFAULT_PORT "27015"
 
-int __cdecl main(int argc, char **argv)
+int main(int argc, char **argv)
 {
 	WSADATA wsaData;
 	SOCKET ConnectSocket = INVALID_SOCKET;
--- Harjoitustehtava_Server/main.cpp
+++ Harjoitustehtava_Server/main.cpp
@@ -2,11 +2,12 @@
 
 #define WIN32_LEAN_AND_MEAN
 
-#include <windows.h>
 #include <winsock2.h>
 #include <ws2tcpip.h>
-#include <stdlib.h>
-#include <stdio.h>
+//#include <stdlib.h>
+//#include <stdio.h>
+
+#include <iostream>
 
 // Need to link with Ws2_32.lib
 #pragma comment (lib, "Ws2_32.lib")
@@ -15,7 +16,7 @@
 #define DEFAULT_BUFLEN 512
 #define DEFAULT_PORT "27015"
 
-int __cdecl main(void)
+int main()
 {
 	WSADATA wsaData;
 	int iResult;
